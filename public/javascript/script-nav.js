4fcaf0384bd7314bbf36f259c141a9c5bdfec44d
Eduard Iacob <B00125295@mytudublin.ie>
2022-04-07 19:48:23 +0100

    different navbar test

--- public/javascript/script-nav.js
+++ public/javascript/script-nav.js
@@ -1,7 +1,16 @@
 // navbar script
-$(document).ready(function () {
-    $('#icon').click(function () {
-        $('ul').toggleClass('show');
-    });
-});
+// $(document).ready(function () {
+//     $('#icon').click(function () {
+//         $('ul').toggleClass('show');
+//     });
+// });
+
+const menu = document.querySelector('#menu-btn');
+const navbar = document.querySelector('.header .navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
 
