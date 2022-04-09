4341b417217875f99c28d3678343f552244fd26e
Eduard Iacob <B00125295@mytudublin.ie>
2022-04-09 11:50:23 +0100

    reset password added for email

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const sqlString = require("sqlstring");
 // const {JWT_SECRET, SENDMAIL_KEY} = require('./configHide');
 const {connection} = require('./dbConnection');
 const {ROLE} = require('./roles');
+const crypto = require('crypto');
 
 dotenv.config({path: './.env'});
 
@@ -31,7 +32,7 @@ var graphRouter = require('./routes/graph');
 var userInfoRouter = require('./routes/userInfo');
 var editDetailsRouter = require('./routes/editDetails')
 var adminRouter = require('./routes/admin');
-
+var resetPassRouter = require('./routes/resetPass');
 
 const HALF_HOUR = 1000 * 60 * 30
 
@@ -81,7 +82,7 @@ app.use('/graph', graphRouter);
 app.use('/userInfo', userInfoRouter);
 app.use('/editDetails', editDetailsRouter);
 app.use('/admin', adminRouter);
-
+app.use('/resetPassword', resetPassRouter);
 
 const transporter = nodeMailer.createTransport(sendGridTransport({
     auth: {
@@ -348,6 +349,204 @@ app.post('/logout', function (req, res) {
     });
 });
 
+app.post('/resetPassword', (req, res) => {
+    var email = req.body.email;
+
+    //create a random token every time
+    crypto.randomBytes(32, (error, buffer) => {
+        if (error) {
+            console.log(error)
+        }
+        //because the token comes in a hex form we need to convert it to a string
+        const token = buffer.toString("hex");
+        const date = Date();
+        connection.query('SELECT email from users WHERE email = ?', [email], (error, result) => {
+            if (result.length === 0) {
+                return res.status(422).json({error: "This email is not associated with any account!"});
+            }
+            connection.query("UPDATE `majorproject`.`users` SET `resetToken` = '" + token + "' WHERE `email` = '" + email + "';", function (error, results, fields) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    transporter.sendMail({
+                        to: result[0].email,
+                        from: "iacobedy2001@gmail.com",
+                        subject: "Password Reset",
+                        html: `<p>You have requested for a password reset for your TripClick account.</p>` +
+                            `<h5>Click on this <a href="http://localhost:3000/resetPassword/${token}">link</a> to reset your password</h5>`
+                    })
+                    res.json({message: "Check your email!"})
+                }
+            });
+        });
+    });
+});
+
+app.get('/resetPassword/:token', function (req, res) {
+    res.send("<!DOCTYPE html>\n" +
+        "<html lang=\"en\" dir=\"ltr\">\n" +
+        "<head>\n" +
+        "    <title>Reset Password</title>\n" +
+        "</head>\n" +
+        "\n" +
+        "<body>\n" +
+        "<div class=\"resetPassword-page\" id=\"resetPassword\">\n" +
+        "    <div class=\"wrapper\">\n" +
+        "        <div class=\"title\">\n" +
+        "            Reset Password\n" +
+        "        </div>\n" +
+        "        <div class=\"form\">\n" +
+        `            <form action=\"/newPassword/${req.params.token}\" method=\"POST\">\n` +
+        "                <div class=\"input-field\">\n" +
+        "                    <label for=\"password\">New password</label>\n" +
+        "                    <input type=\"password\" id=\"password\" class=\"input\" name=\"password\" pattern=\"{6,50}\"\n" +
+        "                           title=\"Password must be at least 6 characters long!!\" required>\n" +
+        "                </div>\n" +
+        "                <div class=\"input-field\">\n" +
+        "                    <label for=\"passwordConfirm\">Confirm new password</label>\n" +
+        "                    <input type=\"password\" id=\"passwordConfirm\" class=\"input\" name=\"passwordConfirm\" pattern=\"{6,50}\"\n" +
+        "                           title=\"Password must be at least 6 characters long!!\" required>\n" +
+        "                </div>\n" +
+        "                <div class=\"button-div\">\n" +
+        "                    <input type=\"submit\" id=\"register-button\" value=\"Reset Password\" class=\"register-button\">\n" +
+        "                </div>\n" +
+        "            </form>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "</div>\n" +
+        "\n" +
+        "</body>\n" +
+        "</html>" +
+        "" +
+        "<style>" +
+        "* {\n" +
+        "    padding: 0;\n" +
+        "    margin: 0;\n" +
+        "    text-decoration: none;\n" +
+        "    list-style: none;\n" +
+        "    font-family: 'Montserrat', sans-serif;\n" +
+        "    box-sizing: border-box;\n" +
+        "\n" +
+        "}\n" +
+        ".wrapper {\n" +
+        "    max-width: 800px;\n" +
+        "    width: 100%;\n" +
+        "    margin: 15px auto;\n" +
+        "    box-shadow: 2px 1px 20px rgba(0, 0, 0, 0.125);\n" +
+        "    padding: 20px;\n" +
+        "}\n" +
+        "\n" +
+        ".wrapper .title {\n" +
+        "    font-size: 20px;\n" +
+        "    font-weight: bold;\n" +
+        "    margin-bottom: 20px;\n" +
+        "    color: cadetblue;\n" +
+        "    text-transform: uppercase;\n" +
+        "    text-align: center;\n" +
+        "}\n" +
+        "\n" +
+        ".wrapper .form {\n" +
+        "    padding: 18px;\n" +
+        "    border: 2px solid cadetblue;\n" +
+        "    width: 100%;\n" +
+        "}\n" +
+        "\n" +
+        ".wrapper .form .input-field {\n" +
+        "    margin-bottom: 10px;\n" +
+        "    align-items: center;\n" +
+        "    position: relative;\n" +
+        "    width: 100%;\n" +
+        "}\n" +
+        "\n" +
+        ".wrapper .form .input-field label {\n" +
+        "    width: 150px;\n" +
+        "    color: #757575;\n" +
+        "    /*margin-right: 10px;*/\n" +
+        "    font-size: 17px;\n" +
+        "    margin-bottom: 2px;\n" +
+        "}\n" +
+        ".wrapper .form .input-field .input:focus {\n" +
+        "    border: 1px solid cadetblue;\n" +
+        "}" +
+        ".wrapper .form .input-field .input {\n" +
+        "    width: 100%;\n" +
+        "    outline: none;\n" +
+        "    border: 1px solid #d5dbd9;\n" +
+        "    font-size: 15px;\n" +
+        "    padding: 8px 10px;\n" +
+        "    border-radius: 3px;\n" +
+        "    transition: all 0.3s ease;\n" +
+        "}\n" +
+        "\n" +
+        ".wrapper .form .input-field {\n" +
+        "    width: 100%;\n" +
+        "    resize: none;\n" +
+        "}" +
+        ".wrapper .form .input-field {\n" +
+        "    font-size: 14px;\n" +
+        "\n" +
+        "}\n" +
+        "\n" +
+        ".wrapper .form .button-div {\n" +
+        "    text-align: center;\n" +
+        "    padding-top: 5px;\n" +
+        "}\n" +
+        "\n" +
+        ".wrapper .form .button-div .register-button {\n" +
+        "    padding: 6px 12px;\n" +
+        "    font-size: 22px;\n" +
+        "    border: 0px;\n" +
+        "    background: cadetblue;\n" +
+        "    color: #fff;\n" +
+        "    cursor: pointer;\n" +
+        "    border-radius: 3px;\n" +
+        "    outline: none;\n" +
+        "}\n" +
+        "\n" +
+        ".wrapper .form .button-div .register-button:hover {\n" +
+        "    background: #ffd658;\n" +
+        "}\n" +
+        "\n" +
+        ".wrapper .form .input-field:last-child {\n" +
+        "    margin-bottom: 0;\n" +
+        "}\n" +
+        "\n" +
+        "@media (max-width: 500px) {\n" +
+        "    .wrapper .form .input-field {\n" +
+        "        flex-direction: column;\n" +
+        "        align-items: flex-start;\n" +
+        "    }\n" +
+        "\n" +
+        "    .wrapper .form .input-field label {\n" +
+        "        margin-bottom: 5px;\n" +
+        "    }\n" +
+        "} " +
+        "</style>");
+});
+
+app.post('/newPassword/:token', function (req, res) {
+    const newPassword = req.body.password;
+    const sentToken = req.params.token;
+    // console.log(sentToken);
+    // console.log(newPassword)
+    connection.query('SELECT resetToken from users WHERE resetToken = ?', [sentToken], async (error, result) => {
+        if (result.length === 0) {
+            return res.status(422).json({error: "Try again session expired!"});
+        } else {
+            let hashedNewPassword = await bcryptjs.hash(newPassword, 10);
+
+            connection.query("UPDATE `majorproject`.`users` SET `password` = '" + hashedNewPassword + "' WHERE `resetToken` = '" + sentToken + "';", function (error, results, fields) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    res.redirect('/login');
+                    // console.log(sentToken);
+                    // console.log(newPassword);
+                }
+            });
+        }
+    });
+});
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     next(createError(404));
---
+++ routes/resetPass.js
@@ -0,0 +1,23 @@
+var express = require('express');
+const session = require("express-session");
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+
+    res.render('resetPassword', {
+        title: 'Reset Password',
+        userId: req.session.userId,
+        username: req.session.username,
+        firstname: req.session.firstname,
+        lastname: req.session.lastname,
+        email: req.session.email,
+        phone: req.session.phone,
+        gender: req.session.gender,
+        country: req.session.country,
+        city: req.session.city,
+        dateRegister: req.session.dateRegister,
+        role: req.session.role
+    });
+});
+
+module.exports = router;
