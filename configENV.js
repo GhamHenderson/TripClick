252b0269b9038d82cd195918a21a66402e55803c
GhamHenderson <B00130193@mytudublin.ie>
2022-04-29 17:44:38 +0100

    Trial Map on index Page

---
+++ configENV.js
@@ -0,0 +1,6 @@
+
+USER=graham
+PASSWORD=AVNS_7I8nfEYH7wgYXUU
+HOST=tripclickdatabase-do-user-11143588-0.b.db.ondigitalocean.com
+DATABASE=majorproject
+SENDMAIL_KEY:"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"
